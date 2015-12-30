01368cc32646b6492bc21e7a6f886d6eb449938e
kxz <1262268789@qq.com>
2015-12-30 15:50:54 +0800

    修改了三个文件

--- SuiQingLong/SuiQingLong/ViewController.m
+++ SuiQingLong/SuiQingLong/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "PersonViewController.h"
+
+
 
 @interface ViewController ()
 
@@ -20,8 +23,16 @@
     
     self.view.backgroundColor = [UIColor blueColor];
     
+    UIBarButtonItem *rightBar = [[UIBarButtonItem alloc] initWithTitle:@"下一页" style:UIBarButtonItemStylePlain target:self action:@selector(abc)];
+    self.navigationItem.rightBarButtonItem = rightBar;
     
-    
+}
+
+
+- (void)abc
+{
+    PersonViewController *p = [[PersonViewController alloc] init];
+    [self.navigationController pushViewController:p animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
