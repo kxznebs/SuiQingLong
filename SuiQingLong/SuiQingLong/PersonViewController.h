60f3814096474ed8372bb9fe536b13241778b2e2
kxz <1262268789@qq.com>
2015-12-30 15:38:34 +0800

    添加了PersonViewController文件

---
+++ SuiQingLong/SuiQingLong/PersonViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonViewController.h
+//  SuiQingLong
+//
+//  Created by xiaohuyangche on 15/12/30.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonViewController : UIViewController
+
+@end
